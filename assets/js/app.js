eef93dc552622e494933be3fd2c6f874a7a34b83
josepicoli <josepicoli42@gmail.com>
2023-08-09 21:16:27 -0300

    restructuring the page

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,22 +7,22 @@ function roll() {
 
     switch(diceNumber) {
         case 1:
-            dice.src = "imgs/dice-1.png"
+            dice.src = "assets/images/dice-1.png"
             break
         case 2:
-            dice.src = "imgs/dice-2.png"
+            dice.src = "assets/images/dice-2.png"
             break
         case 3:
-            dice.src = "imgs/dice-3.png"
+            dice.src = "assets/images/dice-3.png"
             break
         case 4:
-            dice.src = "imgs/dice-4.png"
+            dice.src = "assets/images/dice-4.png"
             break
         case 5:
-            dice.src = "imgs/dice-5.png"
+            dice.src = "assets/images/dice-5.png"
             break
         case 6:
-            dice.src = "imgs/dice-6.png"
+            dice.src = "assets/images/dice-6.png"
             break
         default:
             console.log("ERRO")
